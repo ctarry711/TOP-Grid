c9e5e52d7581eeef3e152dd2a13366cd706f1070
Chris Tarry <ctarry711@gmail.com>
2021-12-15 14:18:06 -0800

    Change input to slider; make colorful

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let gridSize = 16;
 let boxSize = "32px";
+let HSLIndex = 0;
 
 const containerElement = document.querySelector("#container");
+const input = document.querySelector("input");
+
+containerElement.addEventListener("mousemove", () => (HSLIndex += 1));
 
 function generateBoxes(gridSize) {
   boxSize = containerElement.offsetWidth / gridSize;
@@ -9,36 +13,21 @@ function generateBoxes(gridSize) {
   containerElement.style[
     "grid-template-columns"
   ] = `repeat(${gridSize}, ${boxSize}px)`;
+  containerElement.style[
+    "grid-template-rows"
+  ] = `repeat(${gridSize}, ${boxSize}px)`;
   for (i = 0; i < gridSize ** 2; i++) {
     newBox = document.createElement("div");
     newBox.classList.add("box");
-    newBox.style.width = boxSize + "px";
-    newBox.style.height = boxSize + "px";
     containerElement.appendChild(newBox);
   }
 
   boxes = document.querySelectorAll(".box");
   boxes.forEach((box) => {
     box.addEventListener("mouseover", (e) => {
-      box.classList.add("box-hover");
+      box.style["background-color"] = `hsl(${HSLIndex % 360}, 100%, 75%)`;
     });
   });
-}
-
-function getInput() {
-  input = parseInt(prompt("Please choose a gridsize"));
-  console.log(input);
-  if (input === null) {
-    return; //break out of the function early
-  } else if (isNaN(input)) {
-    alert("That is not a number!");
-    input = getInput();
-  } else if (input > 100 || input < 1) {
-    alert("Please choose a number between 1 and a 100");
-    input = getInput();
-  } else {
-    return input;
-  }
 }
 
 generateBoxes(gridSize);
@@ -46,7 +35,6 @@ generateBoxes(gridSize);
 resetButton = document.querySelector("button");
 resetButton.addEventListener("click", (e) => {
   boxes.forEach((box) => box.classList.remove("box-hover"));
-  gridSize = getInput();
+  gridSize = input.value;
   generateBoxes(gridSize);
-  console.log(gridSize);
 });
